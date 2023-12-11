c2d2df218f73558441e47883e2b1f0ecdc1ac0ea
Andrea Sala <andrea.sala98@gmail.com>
2023-12-11 19:22:31 +0100

    Day 11

--- src/11.py
+++ src/11.py
@@ -11,15 +11,15 @@ def manhattan(g1, g2):
 
     return np.sum(np.abs(g1 - g2))
 
-def part2():
-
+def part1():
     out = 0
 
     with open(path, 'r') as f:
         lines = f.read().split('\n')
         
     N, M = len(lines), len(lines[0])
-    G = np.zeros((N, M), dtype=bool)
+    G = np.zeros((N, M), dtype=int)
+    print(N, M)
 
     for i in range(N):
         for j in range(M):
@@ -27,7 +27,48 @@ def part2():
                 G[i][j]=1
 
     G_old = G
-    print(G.shape)
+
+    n_ins = 0
+    for i in trange(N):
+        if np.sum(G_old[i])==0:
+            G = np.insert(G, i+n_ins, np.zeros((M)), 0)
+            n_ins += 1
+
+    n_ins = 0
+    N = len(G)
+
+    for j in trange(M):
+        if np.sum(G_old[:, j])==0:
+            G = np.insert(G, j+n_ins, np.zeros((N)), 1)
+            n_ins += 1
+
+    galaxies = np.argwhere(G==1)
+
+
+    for g1, g2 in combinations(galaxies, 2):
+
+        out += manhattan(g1, g2)
+
+    print(out)
+
+
+def part2():
+
+    out = 0
+
+    with open(path, 'r') as f:
+        lines = f.read().split('\n')
+
+    N, M = len(lines), len(lines[0])
+    G = np.zeros((N, M), dtype=int)
+
+    for i in range(N):
+        for j in range(M):
+            if lines[i][j]=='#':
+                G[i][j]=1
+
+    G_old = G
+
     n_ins = 0
     for i in trange(N):
         if np.sum(G_old[i])==0:
@@ -35,31 +76,39 @@ def part2():
                 n_ins += N_EXP
 
     N = len(G)
-    print(G.shape)
+
     n_ins = 0
 
-    G = G.T
+    G_new = G_old.T
 
     for i in trange(M):
         if np.sum(G_old[:, i])==0:
-            G = np.insert(G, i+n_ins, np.zeros((N_EXP, N)), 0)
+            G_new = np.insert(G_new, i+n_ins, np.zeros((N_EXP, len(G_old)), dtype=int), 0)
             n_ins += N_EXP
 
-    G = G.T
+    G_new = G_new.T
     M = len(G[0])
-    print(G.shape)
+
+    # print(G)
+    # print(G_new)
+
     galaxies = np.argwhere(G==1)
 
     for g1, g2 in combinations(galaxies, 2):
-        out += manhattan(g1, g2)
+        # print(g1, g2, abs(g1[1] - g2[1]))
+        out += abs(g1[0] - g2[0])
 
+    galaxies2 = np.argwhere(G_new==1)
+
+    for g1, g2 in combinations(galaxies2, 2):
+        out += abs(g1[1] - g2[1])
+        
 
     print(out)
 
 
 
-
-
 if __name__ == '__main__':
 
+    part1()
     part2()
